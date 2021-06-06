a515a9025568795e9c2e1acf93518d8e3d7ab561
zaanposni <me@zaanposni.com>
2021-06-06 10:16:59 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+<h1>👮‍♂️ MASZ API Wrapper</h1>
+<p>
+<img src="https://img.shields.io/pypi/pyversions/masz" />
+<img src="https://img.shields.io/pypi/v/masz" />
+<a href="https://github.com/zaanposni/masz-api-wrapper/blob/dev/LICENSE"><img src="https://img.shields.io/github/license/zaanposni/masz-api-wrapper.svg"/></a>
+</p>
+
+⭐ **Fully object-oriented library** to integrate the <a href="https://github.com/zaanposni/discord-masz" target="_blank">**MASZ API**</a> into your project.
+
+## 🚀 Installation
+
+**Python 3.6 or higher required**
+```
+pip install masz
+```
+
+## 👀Examples
+
+
+## 🤝 Contributors <img src="https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat"/>
+
+<a href="https://github.com/zaanposni"><img src="https://avatars3.githubusercontent.com/u/24491035?s=460&v=4"
+                                            height=90px, width=90px style="border-radius: 50%" /></a>
+
+## License:
+
+This project is licensed under MIT.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name='masz',
+  py_modules=["masz"],
+  version='1.0',
+  license='MIT',
+  description='API Wrapper for MASZ (A discord moderation bot)',
+  author='zaanposni',
+  author_email='me@zaanposni.com',
+  url='https://github.com/zaanposni/masz-api-wrapper',
+  keywords=['MASZ', 'API', 'DISCORD', 'WRAPPER', 'JSON', 'REST', 'MODERATION', 'TOKEN', 'PYTHON'],
+  packages=find_packages(exclude=["*tests"]),
+  package_data={
+    "masz": ["masz/*"]
+  },
+  install_requires=[
+          'requests',
+          'typing',
+      ],
+  classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+  long_description=long_description,
+  long_description_content_type="text/markdown"
+)
