539840dc4a3921b005e5f74c8a5cfa15da8f6434
zaanposni <me@zaanposni.com>
2021-07-30 10:49:12 +0200

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 setup(
   name='masz',
   py_modules=["masz"],
-  version='1.1.0',
+  version='1.1.1',
   license='MIT',
   description='API Wrapper for MASZ (A discord moderation bot)',
   author='zaanposni',
